97049d70792b20ad53128e20ba2230a537087b5e
Rokostom <togglermap@gmail.com>
2023-04-12 21:18:34 +0600

    feat: add simple header code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetails />} />
+            <Route element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
