268154e39624fe8c9e943e59f39fa6f08f65aa0d
Rokostom <togglermap@gmail.com>
2023-04-14 13:47:23 +0600

    nvjn

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -17,7 +17,7 @@ const MovieDetail = () => {
     return (
         <div className="movie-section">
             {Object.keys(data).length === 0 ?
-            (<div>...Loadign</div>)
+            (<div>...Loading</div>)
             : (
             <>
             <div className="section-left">
